c5ce2ad8a3a43746ba3204e678c89b8539c618e6
Anton P <polevyy.anton@gmail.com>
2019-11-13 03:00:59 +1100

    +mutation register() to register new user
    mutation{
      register(registerInput:{
        username:"user"
        password:"123456"
        confirmPassword: "123456"
        email:"user@email.com"
      }) {
        id
        email
        token
        username
        createdAt
      }
    }

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://user123:Passw0rd@cluster0-ulbp7.mongodb.net/merng?retryWrites=true&w=majority'
+    MONGODB: 'mongodb+srv://user123:Passw0rd@cluster0-ulbp7.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
 }
