bb7a2266f25502516f90cbb2ca1e5d854bac451b
Anton P <polevyy.anton@gmail.com>
2019-11-13 01:14:30 +1100

    Connect to MongoDB *(config.js will be ignored in next commit)
    * in config.js use valid 'username:password' in MONGODB connection string and hide it from git commits

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://user1:P@ssw0rd@cluster0-ulbp7.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config.js');
 
 // here go all GraphQL types
 const typeDefs = gql`
     type Query{
         sayHi: String!
     }
-`
+`;
 
 const resolvers = {
     Query: {
         sayHi: () => 'Hello World!'
     }
-}
+};
 
 // ApolloServer runs Express behind the scenes
 const server = new ApolloServer({
     typeDefs,
     resolvers
-})
+});
 
 const PORT = process.env.PORT || 5000;
 
-server.listen({ port: PORT })
-    .then(res => {
-        console.log(`Server running at ${res.url}`)
+mongoose.connect(MONGODB, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+    .then(() => {
+        console.log('MongoDB connected');
+        return server.listen({ port: PORT })
     })
+    .then(res => {
+        console.log(`Server running at ${res.url}`);
+    });
