fe87b70190e1659c6e488f417f543c1366f6acf2
Anton P <polevyy.anton@gmail.com>
2019-11-14 22:41:45 +1100

    add Routes '/home', '/login', '/register'

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      <h1>Hi world!</h1>
-    </div>
+    <BrowserRouter>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </BrowserRouter>
   );
 }
 
