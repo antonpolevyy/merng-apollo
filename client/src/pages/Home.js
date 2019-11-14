9174d69514b33f8cabd7b5c82de383e05b2903ca
Anton P <polevyy.anton@gmail.com>
2019-11-15 00:19:42 +1100

    Home: fetch getPosts() and console.log(data)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts{
+      id 
+      body 
+      createdAt 
+      username 
+      likeCount 
+      likes{
+        username
+      }
+      commentCount 
+      comments{
+        id 
+        username 
+        createdAt 
+        body
+      }
+    }
+  }
+`;
 
 function Home() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+  if(data){
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
 export default Home;
