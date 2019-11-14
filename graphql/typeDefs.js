bab748bdb96c7da84fd914f7ae7a704080248385
Anton P <polevyy.anton@gmail.com>
2019-11-14 18:30:28 +1100

    + mutation likePost()
    mutation{
      likePost(postId:"..."){
        id
        body
        likes{
          id
          username
        }
      }
    }

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -9,7 +9,7 @@ module.exports = gql`
         username: String!
         createdAt: String!
         comments: [Comment]!
-        # likes: [Like]!
+        likes: [Like]!
     }
     type Comment{
         id: ID!
@@ -17,11 +17,11 @@ module.exports = gql`
         username: String!
         body: String!
     }
-    # type Like{
-    #     id: ID!
-    #     createdAt: String!
-    #     username: String!
-    # }
+    type Like{
+        id: ID!
+        createdAt: String!
+        username: String!
+    }
     type User{
         id: ID!
         email: String!
@@ -46,6 +46,6 @@ module.exports = gql`
         deletePost(postId: ID!): String!
         createComment(postId: ID!, body: String!): Post!
         deleteComment(postId:ID!, commentId: ID!): Post!
-        # likePost(postId: ID!): Post!
+        likePost(postId: ID!): Post!
     }
 `;
