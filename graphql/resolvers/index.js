fbe66ccef56109d21cdc5cf80831e51e2ff3d353
Anton P <polevyy.anton@gmail.com>
2019-11-14 19:15:05 +1100

    + likeCount + commentCount

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,17 @@ const usersResolvers = require('./users');
 const commentsResolvers = require('./comments');
 
 module.exports = {
+    // likeCount & commentCount could be updated in mutations instead
+    // but here we use mutation to do that (if the term 'mutation' is right)
+    Post: {
+        // each time any query, mutation or subscription returns typeDef 'Post'
+        // next operation will be called:
+        likeCount: (parent) => {
+            console.log(parent);
+            return parent.likes.length;
+        },
+        commentCount: (parent) => parent.comments.length
+    },
     Query: {
         ...postsResolvers.Query
     },
