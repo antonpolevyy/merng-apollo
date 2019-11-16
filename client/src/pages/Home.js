f3f8607e06c8490bba113f4c6d95ee64437a687b
Anton P <polevyy.anton@gmail.com>
2019-11-17 02:45:36 +1100

    replace FETCH_POSTS_QUERY in separate file (to use in different components)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,11 @@
 import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
 import { Grid } from 'semantic-ui-react';
 
 import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts{
-      id 
-      body 
-      createdAt 
-      username 
-      likeCount 
-      likes{
-        username
-      }
-      commentCount 
-      comments{
-        id 
-        username 
-        createdAt 
-        body
-      }
-    }
-  }
-`;
+import { FETCH_POSTS_QUERY } from '../util/graphql'
 
 function Home() {
   const { user } = useContext(AuthContext);
